8c3c8c764399f1ab1bcc03c8e4a70ed32d23e6ab
Luke Rodgers <lukerodgers90@gmail.com>
2016-02-28 20:10:51 +0000

    Remove unknown command

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,7 +2,7 @@
 
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
   CMAKE_URL="http://www.cmake.org/files/v3.3/cmake-3.3.1-Linux-x86_64.tar.gz"
-  mkdir cmake && travis_retry wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+  mkdir cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
   export PATH=${DEPS_DIR}/cmake/bin:${PATH}
 fi
 
