5788ce1f5baaeb5b4ab14d3f605e095ac6b2e41e
Luke Rodgers <lukerodgers90@gmail.com>
2016-02-28 20:26:28 +0000

    Attempt cmake update travis

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-echo $PATH
-which cmake
+echo $SPECIFIC_CMAKE
+
 cmake --version
 make --version
 if [ -d build ]; then
@@ -8,6 +8,12 @@ if [ -d build ]; then
 fi
 mkdir build
 cd build
-cmake ..
+
+if [! -z "$var" ]; then
+    (${SPECIFIC_CMAKE} ..)
+else
+    cmake ..
+fi
+
 make
 ./../bin/runTests
