75427dd6c86a7b5bcf18b42724454527aa0e9947
Luke Rodgers <lukerodgers90@gmail.com>
2016-03-12 15:56:12 +0000

    Refactored node logic to be generic
    And use an unordered map. (HashMap?)

--- src/node.h
+++ src/node.h
@@ -2,60 +2,93 @@
 #define MAZES_FOR_PROGRAMMERS_NODE_H
 
 #include <stdexcept>
+#include <unordered_map>
+#include <string>
 
 class Node {
 
-    Node *x_plus = nullptr;
-    Node *x_minus = nullptr;
+private:
+    const std::string positive = "positive";
+    const std::string negative = "negative";
+
+    std::unordered_map<std::string, Node*> x_axis;
 
 public:
     void setRightPtr(Node *node) {
-        Node *currentPtr = this->x_plus;
-        if (currentPtr == node) {
-            return;
-        }
-        this->x_plus = node;
-
-        if (currentPtr != nullptr) {
-            currentPtr->setLeftPtr(nullptr);
-        }
-
-        if (node == nullptr) {
-            return;
-        }
-
-        Node *ptr = node->getLeftPtr();
-
-        if (this->shouldUpdatePtr(ptr)) {
-            node->setLeftPtr(this);
-        }
+        this->setPtr(node, this->positive);
     }
 
-public:
+    Node *getRightPtr() {
+        return this->getPtr(this->positive);
+    }
+
     void setLeftPtr(Node *node) {
-        Node *currentPtr = this->x_minus;
-        if (currentPtr == node) {
-            return;
-        }
-        this->x_minus = node;
+        this->setPtr(node, this->negative);
+    }
 
-        if (currentPtr != nullptr) {
-            currentPtr->setRightPtr(nullptr);
-        }
-
-
-        if (node == nullptr) {
-            return;
-        }
-
-        Node *ptr = node->getRightPtr();
-
-        if (this->shouldUpdatePtr(ptr)) {
-            node->setRightPtr(this);
-        }
+    Node *getLeftPtr() {
+        return this->getPtr(this->negative);
     }
 
 private:
+
+    void setPtr(Node *node, std::string direction){
+        Node *currentPtr = this->getPtr(direction);
+        if (currentPtr == node) {
+            return;
+        }
+
+        this->safeInsert(direction, node);
+
+        std::string oppositeDirection = this->getOppositeDirection(direction);
+
+        /**
+         * Break the link with the current sister node.
+         */
+        if (currentPtr != nullptr) {
+            currentPtr->setPtr(nullptr, oppositeDirection);
+        }
+
+        if (node == nullptr) {
+            return;
+        }
+
+        /**
+         * Link the new sister node
+         */
+        Node *ptr = node->getPtr(oppositeDirection);
+
+        if (this->shouldUpdatePtr(ptr)) {
+            node->setPtr(this, oppositeDirection);
+        }
+    }
+
+    Node *getPtr(std::string direction) {
+        if (this->x_axis.count(direction)) {
+            return this->x_axis.at(direction);
+        } else {
+            return nullptr;
+        }
+    }
+
+    void safeInsert(std::string direction, Node *node)
+    {
+        if (this->x_axis.count(direction)) {
+            this->x_axis.erase(direction);
+        }
+        this->x_axis.insert({direction, node});
+    }
+
+    std::string getOppositeDirection(std::string direction) {
+        if (direction == this->positive) {
+            return this->negative;
+        } else if (direction == this->negative) {
+            return this->positive;
+        } else {
+            throw std::logic_error("Unexpected axis direction given");
+        }
+    }
+
     bool shouldUpdatePtr(Node *ptr) {
         if (ptr == this) {
             //Already paired
@@ -67,16 +100,6 @@ private:
         } else {
             throw std::logic_error("Should pair with something else");
         }
-    }
-
-public:
-    Node *getRightPtr() {
-        return this->x_plus;
-    }
-
-public:
-    Node *getLeftPtr() {
-        return this->x_minus;
     }
 };
 
