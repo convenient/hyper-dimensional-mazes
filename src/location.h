736f2a68531ce8cd80a70de144c6ac5010112cad
Luke Rodgers <lukerodgers90@gmail.com>
2016-03-19 18:08:52 +0000

    Create basic location comparing functionality

--- src/location.h
+++ src/location.h
@@ -5,19 +5,56 @@
 #include <map>
 #include <sstream>
 #include <stdexcept>
+#include <cmath>
+#include <vector>
 
 class Location {
-private:
-    std::map<std::string, int> map;
-    std::string location = "";
-
 public:
-    void addPoint(std::string identifier, int position) {
-        if (this->map.count(identifier)) {
-            throw std::logic_error("Tried add identifier where already exists");
+    std::string location = "";
+    std::map<std::string, int> map;
+    std::map<std::string, std::string> defined_axis;
+
+    std::map<std::string, std::string> getDefinedAxis() {
+        return this->defined_axis;
+    }
+
+    void addPoint(std::string axisIdentifier, int position) {
+        if (this->map.count(axisIdentifier)) {
+            throw std::logic_error("Tried to add a point when axis already defined");
         }
-        this->map.insert({identifier, position});
+        this->map.insert({axisIdentifier, position});
+        this->defined_axis.insert({axisIdentifier, axisIdentifier});
         this->location = "";
+    }
+
+    int getPositionOnAxis(std::string axisIdentifier) {
+        if (!this->map.count(axisIdentifier)) {
+            return 0;
+        }
+
+        return this->map.at(axisIdentifier);
+    }
+
+    double getEuclideanDistanceTo(Location location) {
+
+        // d(a,b) = sqrt( (a1-b1)^2 + (a2-b2)^2 + (a3-b3)^2 + (a4-b4)^2 )
+
+        double distance = 0;
+
+        std::vector<std::string> axis = Location::getAllAxis(this[0], location);
+
+        for(std::vector<std::string>::reverse_iterator it = axis.rbegin(); it != axis.rend(); ++it) {
+            std::string axisIdentifier = *it;
+
+            int position = this->getPositionOnAxis(axisIdentifier);
+            int comparePosition = location.getPositionOnAxis(axisIdentifier);
+
+            distance  += pow((position - comparePosition), 2);
+        }
+
+        distance = sqrt(distance);
+
+        return distance;
     }
 
     std::string getAsString() {
@@ -32,6 +69,38 @@ public:
         }
 
         return this->location;
+    }
+
+    static std::vector<std::string> getAllAxis(Location a, Location b){
+
+        std::map<std::string, std::string> allDefinedAxis;
+
+        //Get all the axis defined in Location a.
+        for (auto i : a.getDefinedAxis()) {
+            std::string axisIdentifier = i.first;
+            if (!allDefinedAxis.count(axisIdentifier)) {
+                allDefinedAxis.insert({axisIdentifier, axisIdentifier});
+            }
+        }
+
+        //Get all the axis defined in Location b.
+        std::map<std::string, std::string> locDefinedAxis = b.getDefinedAxis();
+        for (auto i : locDefinedAxis) {
+            std::string axisIdentifier = i.first;
+            if (!allDefinedAxis.count(axisIdentifier)) {
+                allDefinedAxis.insert({axisIdentifier, axisIdentifier});
+            }
+        }
+
+        //Extract them from this temporary map and pop them in vector.
+        std::vector<std::string> axis;
+
+        for (auto i: allDefinedAxis) {
+            std::string axisIdentifier = i.first;
+            axis.push_back(axisIdentifier);
+        }
+
+        return axis;
     }
 };
 
--- tests/src/location.cpp
+++ tests/src/location.cpp
@@ -15,3 +15,46 @@ TEST(location_test, get_location_as_string) {
     a.addPoint("z", 99);
     ASSERT_EQ("(x:1)(y:9)(z:99)", a.getAsString());
 }
+
+TEST(location_test, euclidean_distance_basic) {
+
+    Location a;
+    a.addPoint("x", 1);
+
+    Location b;
+    b.addPoint("x", 6);
+
+    double distance = a.getEuclideanDistanceTo(b);
+
+    ASSERT_EQ(5, distance);
+}
+
+TEST(location_test, euclidean_distance_backwards) {
+
+    Location a;
+    a.addPoint("a", 5);
+
+    Location b;
+    b.addPoint("a", 0);
+
+    double distance = a.getEuclideanDistanceTo(b);
+
+    ASSERT_EQ(5, distance);
+}
+
+
+TEST(location_test, euclidean_distance_two_dimensional) {
+
+    //These two points form a pretty typical 3/4/5 triangle.
+    Location a;
+    a.addPoint("a", 5);
+    a.addPoint("b", 5);
+
+    Location b;
+    b.addPoint("a", 9); //4 difference
+    b.addPoint("b", 8); //3 difference
+
+    double distance = a.getEuclideanDistanceTo(b);
+
+    ASSERT_EQ(5, distance);
+}
