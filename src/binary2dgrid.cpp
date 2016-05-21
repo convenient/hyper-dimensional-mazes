a8a4aa1d236cb587011d399b9ac51b566464df9d
Luke Rodgers <lukerodgers90@gmail.com>
2016-05-21 18:27:31 +0100

    Extract out path finding etc into handler.h

--- src/binary2dgrid.cpp
+++ src/binary2dgrid.cpp
@@ -1,123 +1,12 @@
 #include <iostream>
-#include <chrono>
-#include <thread>
-#include <future>
 #include "mazebinary.h"
 #include "RendererGrid2D.h"
-#include "dijkstra.h"
-
 MazeBinary maze;
 Maze* mazePtr = &maze;
-bool mazeSolved = false;
-
+#include "handler.h"
 
 void render() {
 
-}
-
-std::vector<Node *> getLongestPath(Node *start, std::vector<Node *> endPoints) {
-    Dijkstra dijkstraSolver;
-
-    std::vector<Node *> longestPath;
-
-    std::unordered_map<Node *, std::vector<Node *>> potentialSolvedPaths =
-            dijkstraSolver.getPath(start, endPoints);
-    for (auto potentialSolvedPath : potentialSolvedPaths) {
-        std::vector<Node *> potentialPath = potentialSolvedPath.second;
-        if (potentialPath.size() > longestPath.size()) {
-            longestPath = potentialPath;
-        }
-    }
-
-    return longestPath;
-}
-
-std::vector<Node *> getLongestPathFromBatches(std::unordered_map<Node *, std::vector<Node *>> batches) {
-    std::vector<Node *> longestPath;
-
-    int batchSize = 100;
-
-    while (!batches.empty()) {
-
-        std::vector<std::future<std::vector<Node *>>> futuresSolutions;
-        int counter = 0;
-
-        std::vector<Node *> batchesToRemove;
-
-        for (auto batch : batches) {
-            Node * start = batch.first;
-            std::vector<Node *> endPoints = batch.second;
-
-            batchesToRemove.push_back(start);
-            futuresSolutions.push_back(std::async(std::launch::async, &getLongestPath, start, endPoints));
-
-            if (counter++ == batchSize) {
-                break;
-            }
-        }
-
-        for (std::size_t i = 0; i != futuresSolutions.size(); ++i) {
-            std::vector<Node *> potentialLongestPath = futuresSolutions[i].get();
-            if (potentialLongestPath.size() > longestPath.size()) {
-                longestPath = potentialLongestPath;
-            }
-        }
-
-        for (auto batchToRemove : batchesToRemove) {
-            batches.erase(batchToRemove);
-        }
-    }
-
-    return longestPath;
-}
-
-void solve() {
-    if (mazeSolved) {
-        return;
-    }
-    std::vector<Node *> deadEnds = maze.getDeadEnds();
-    if (deadEnds.size() < 2) {
-        std::cout << "Er....not enough dead ends to solve" << std::endl;
-        return;
-    }
-    std::cout << "Solving" << std::endl;
-
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    std::unordered_map<Node *, std::vector<Node *>> batches;
-
-    /**
-     * Not quite All Pairs All Paths, but since A->B is the same path as B->A for shortest paths
-     * This is a good approach to half the amount of paths to be calculated.
-     *
-     * Split them into batches of start node -> many end nodes
-     */
-    for (std::size_t i = 0; i < deadEnds.size(); i++) {
-        Node * start = deadEnds.at(i);
-        std::vector<Node *> endPoints;
-        for (std::size_t j=i+1; j< deadEnds.size(); j++) {
-            Node *end = deadEnds.at(j);
-            endPoints.push_back(end);
-        }
-        batches.insert({start, endPoints});
-    }
-
-    std::vector<Node *> longestPath = getLongestPathFromBatches(batches);
-    batches.clear();
-
-    RendererGrid2D::drawPath(mazePtr, longestPath);
-    mazeSolved = true;
-
-    auto current_time = std::chrono::high_resolution_clock::now();
-    std::cout << "Solution took " << std::chrono::duration_cast<std::chrono::duration<float>>(current_time - start_time).count() << " seconds and has a distance of " << longestPath.size() << std::endl;
-
-}
-
-void generate() {
-    std::cout << "Generating" << std::endl;
-    maze.generate();
-    RendererGrid2D::drawMaze(mazePtr);
-    mazeSolved = false;
 }
 
 void processKeys(unsigned char key, int x, int y)
@@ -127,7 +16,7 @@ void processKeys(unsigned char key, int x, int y)
     switch (charKey)
     {
         case 'q': exit(0); break;
-        case 'g': generate(); break;
+        case 'g': generate(); RendererGrid2D::drawMaze(mazePtr); break;
         case 's': solve(); break;
         default: break;
     }
