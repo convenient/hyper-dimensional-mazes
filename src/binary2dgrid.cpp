29fd4f95b43544b6c90888885a4cb2239f135023
Luke Rodgers <lukerodgers90@gmail.com>
2016-05-21 17:35:52 +0100

    Refactor path finding into batches for threading

--- src/binary2dgrid.cpp
+++ src/binary2dgrid.cpp
@@ -2,6 +2,8 @@
 #include "RendererGrid2D.h"
 #include "dijkstra.h"
 #include <iostream>
+#include <chrono>
+#include <iostream>
 
 MazeBinary maze;
 Maze* mazePtr = &maze;
@@ -10,6 +12,41 @@ bool mazeSolved = false;
 
 void render() {
 
+}
+
+std::vector<Node *> getLongestPath(Node *start, std::vector<Node *> endPoints) {
+    Dijkstra dijkstraSolver;
+
+    std::vector<Node *> longestPath;
+
+    std::unordered_map<Node *, std::vector<Node *>> potentialSolvedPaths =
+            dijkstraSolver.getPath(start, endPoints);
+    for (auto potentialSolvedPath : potentialSolvedPaths) {
+        std::vector<Node *> potentialPath = potentialSolvedPath.second;
+        if (potentialPath.size() > longestPath.size()) {
+            longestPath = potentialPath;
+        }
+    }
+
+    return longestPath;
+}
+
+std::vector<Node *> getLongestPathFromBatches(std::unordered_map<Node *, std::vector<Node *>> batches) {
+    std::vector<Node *> longesPath;
+    int batchSize = 1;
+    int counter = 0;
+
+    for (auto batch : batches) {
+        Node * start = batch.first;
+        std::vector<Node *> endPoints = batch.second;
+
+        std::vector<Node *> potentialLongestPath = getLongestPath(start, endPoints);
+        if (potentialLongestPath.size() > longesPath.size()) {
+            longesPath = potentialLongestPath;
+        }
+    }
+
+    return longesPath;
 }
 
 void solve() {
@@ -22,43 +59,37 @@ void solve() {
         return;
     }
     std::cout << "Solving" << std::endl;
-    Dijkstra dijkstraSolver;
 
-    unsigned long counter = 0;
-    unsigned long todo = ((deadEnds.size() * deadEnds.size())/2) - (deadEnds.size()/2);
+    auto start_time = std::chrono::high_resolution_clock::now();
 
-    std::vector<Node *> longestPath;
+    std::unordered_map<Node *, std::vector<Node *>> batches;
 
     /**
      * Not quite All Pairs All Paths, but since A->B is the same path as B->A for shortest paths
      * This is a good approach to half the amount of paths to be calculated.
+     *
+     * Split them into batches of start node -> many end nodes
      */
-    for (std::size_t i = 0; i < deadEnds.size(); ++i) {
+    for (std::size_t i = 0; i < deadEnds.size(); i++) {
         Node * start = deadEnds.at(i);
         std::vector<Node *> endPoints;
-        for (std::size_t j=i+1; j< deadEnds.size(); ++j) {
+        for (std::size_t j=i+1; j< deadEnds.size(); j++) {
             Node *end = deadEnds.at(j);
             endPoints.push_back(end);
         }
-
-        /**
-         * Dijkstras has been adjusted too, to allow a one:many solution to be computed at the same time.
-         */
-        std::unordered_map<Node *, std::vector<Node *>> potentialSolvedPaths = dijkstraSolver.getPath(start, endPoints);
-        for (auto potentialSolvedPath : potentialSolvedPaths) {
-            std::cout << counter++ << "/" << todo << std::endl;
-            std::vector<Node *> potentialPath = potentialSolvedPath.second;
-            if (potentialPath.size() > longestPath.size()) {
-                longestPath = potentialPath;
-            }
-        }
-
-        std::cout << counter++ << "/" << todo << std::endl;
+        batches.insert({start, endPoints});
     }
+
+    std::vector<Node *> longestPath = getLongestPathFromBatches(batches);
+    batches.clear();
 
     std::cout << longestPath.size() << std::endl;
     RendererGrid2D::drawPath(mazePtr, longestPath);
     mazeSolved = true;
+
+    auto current_time = std::chrono::high_resolution_clock::now();
+    std::cout << "Solution took " << std::chrono::duration_cast<std::chrono::duration<float>>(current_time - start_time).count() << " seconds" << std::endl;
+
 }
 
 void generate() {
@@ -83,7 +114,7 @@ void processKeys(unsigned char key, int x, int y)
 
 int main(int argc, char **argv) {
 
-    int mazeSize = 30;
+    int mazeSize = 25;
     int minpart = (int)floor(mazeSize/2) * -1;
     int maxpart = (int)ceil(mazeSize/2);
 
