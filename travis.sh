4345610b3f32327324b9d4f770948f265dadb9ed
Luke Rodgers <lukerodgers90@gmail.com>
2016-05-31 21:49:37 +0100

    Modify travis script

---
+++ travis.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+cmake --version
+make --version
+if [ -d build ]; then
+    rm -rf build
+fi
+if [ -d bin ]; then
+    rm -rf bin
+fi
+mkdir build
+mkdir bin
+echo "This directory contains all the output binary files." > bin/README.md
+cd build
+#cmake ..
+/home/luker/Applications/clion-1.2.4/bin/cmake/bin/cmake ..
+make
+./../bin/runTests
