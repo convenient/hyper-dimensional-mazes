93a39d1e1ad1864a834f1ea53905ea588db8cacc
Luke Rodgers <lukerodgers90@gmail.com>
2016-08-11 20:10:22 +0100

    Aldous-Broder random walk algorithm

---
+++ src/aldousBroder4d.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "mazealdousbroder.h"
+#include "RendererText.h"
+#include "solver.h"
+
+int main(int argc, char **argv) {
+
+    Maze *mazePtr = new MazeAldousBroder;
+    Solver *solver = new Solver(mazePtr);
+
+    int mazeSize = 6;
+
+    std::cout << "Generating a binary maze of " << mazeSize << " by "<< mazeSize << " by " << mazeSize << " by " << mazeSize <<  std::endl;
+
+    for (int w=0; w< mazeSize; w++) {
+        for (int x = 0; x < mazeSize; x++) {
+            for (int y = 0; y < mazeSize; y++) {
+                for (int z = 0; z < mazeSize; z++) {
+                    Point p;
+                    p.addPosition("w", w);
+                    p.addPosition("x", x);
+                    p.addPosition("y", y);
+                    p.addPosition("z", z);
+                    mazePtr->createNode(p);
+                }
+            }
+        }
+    }
+    mazePtr->generate();
+
+    std::vector<Node *> path = solver->solve();
+    RendererText::drawPath(path);
+
+    return 0;
+}
