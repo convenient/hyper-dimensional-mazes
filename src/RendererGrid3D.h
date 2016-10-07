212dd3c429044ea6dc308cf412c1c5770b973591
Luke Rodgers <lukerodgers90@gmail.com>
2016-10-07 22:08:13 +0100

    Revert "Working 3d rendering of a maze during linkage"
    This reverts commit 2c2a06d06d26e80dc6c3014da9cc7ec6b0af4b61.

--- src/RendererGrid3D.h
+++ src/RendererGrid3D.h
@@ -107,12 +107,10 @@ class RendererGrid3D {
                 xAxisIdentifier = axis.at(0);
                 yAxisIdentifier = axis.at(1);
                 zAxisIdentifier = axis.at(2);
-                this->threeDimensions = true;
             } else if (axis.size() == 2) {
                 xAxisIdentifier = axis.at(0);
                 yAxisIdentifier = axis.at(1);
                 zAxisIdentifier = "totally_not_a_real_axis_identifier";
-                this->threeDimensions = false;
             } else {
                 throw std::logic_error("Tried to render a non-3d or a non-2d maze");
             }
@@ -356,24 +354,10 @@ class RendererGrid3D {
     }
 
     static void linkCallback(Maze *m, Node *a, Node *b) {
-
-        if (superSecretOpenGlHackyPointer->threeDimensions) {
-            unsigned long count = superSecretOpenGlHackyPointer->m->getMap().size();
-            unsigned long interval = count / 100;
-            if (superSecretOpenGlHackyPointer->threeDimensionRotateCount % interval == 0) {
-                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-                superSecretOpenGlHackyPointer->rotationXaxis += (0.075f) * superSecretOpenGlHackyPointer->rotateMultiplier;
-                superSecretOpenGlHackyPointer->rotationYaxis += (0.05f) * superSecretOpenGlHackyPointer->rotateMultiplier;
-
-                glRotatef(superSecretOpenGlHackyPointer->rotationXaxis, 1.0f, 0.0f, 0.0f);
-                glRotatef(superSecretOpenGlHackyPointer->rotationYaxis, 0.0f, 1.0f, 0.0f);
-                glRotatef(superSecretOpenGlHackyPointer->rotationZaxis, 0.0f, 0.0f, 1.0f);
-            }
-            superSecretOpenGlHackyPointer->threeDimensionRotateCount++;
+        
+        for (auto i : m->getVisitedNodesMap()) {
+            superSecretOpenGlHackyPointer->drawNode(i.second);
         }
-
-        superSecretOpenGlHackyPointer->drawMaze();
 
         glutPostRedisplay();
         glFlush();//probably overkill, but it works.
@@ -456,18 +440,13 @@ class RendererGrid3D {
     }
 
 public:
-
-    bool threeDimensions = false;
-    unsigned long threeDimensionRotateCount = 0;
-
     void generate() {
         m->generate();
     }
 
     void generateAndDraw() {
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+        //todo make it clear the screen
         m->generate();
         solver->setMazeUnsolved();
         std::vector<Node *> solution = solver->solve();
