c5d8604c65b99b58bd8b620bf8f1900526da95f4
Luke Rodgers <lukerodgers90@gmail.com>
2016-11-13 20:20:09 +0000

    Add remaining recursive backtracker mazes

---
+++ src/recursivebacktracker.cpp
@@ -0,0 +1,8 @@
+#include "graph/maze/mazerecursivebacktracker.h"
+#include "bootstrap/convenient_maze_textonly.h"
+
+int main(int argc, char **argv) {
+    //Dimensions default    2
+    //Edge length default   18
+    return convenient_maze_textonly::init(argc, argv, new MazeRecursiveBacktracker, "4", "6");
+}
---
+++ src/recursivebacktracker3d.cpp
@@ -0,0 +1,9 @@
+#include "graph/maze/mazerecursivebacktracker.h"
+#include "bootstrap/convenient_maze_opengl.h"
+
+int main(int argc, char **argv) {
+    //Dimensions default    2
+    //Edge length default   18
+    //Render opengl
+    return convenient_maze_opengl::init(argc, argv, new MazeRecursiveBacktracker, "3", "10");
+}
