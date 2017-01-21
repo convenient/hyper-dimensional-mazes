4530842f9dcdf96e0c74afaedfa3748ab07de231
Luke Rodgers <lukerodgers90@gmail.com>
2017-01-21 23:04:01 +0000

    Initial sidewinder 3d

--- readme/sidewinderfinder.sh
+++ readme/sidewinderfinder.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "" > .sidewinderfinder.log
-for i in `seq 1 100000`; do ././../bin/binary_3d -l 6; done > .sidewinderfinder.log
+for i in `seq 1 100000`; do ././../bin/sidewinder_3d -l 7; done > sidewinderfinder.log
